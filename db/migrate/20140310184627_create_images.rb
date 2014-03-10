f95410d87177f284732e434325d14d201962de71
Justin Kenyon <kenyonj@gmail.com>
2014-03-10 14:53:22 -0400

    Add gallery and image migrations

---
+++ db/migrate/20140310184627_create_images.rb
@@ -0,0 +1,10 @@
+class CreateImages < ActiveRecord::Migration
+  def change
+    create_table :images do |t|
+      t.string :name
+      t.text :description
+      t.string :url
+      t.integer :gallery_id
+    end
+  end
+end
