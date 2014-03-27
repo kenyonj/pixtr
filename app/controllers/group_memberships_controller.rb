d6133972a01dbfb86c3ab4ffaaea6def2974281b
Justin Kenyon <kenyonj@gmail.com>
2014-03-27 10:30:42 -0400

    Add ajax for group joining and leaving

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -1,15 +1,13 @@
 class GroupMembershipsController < ApplicationController
   def create
-    group = find_group
-    membership = current_user.join group
+    @group = find_group
+    membership = current_user.join @group
     process_activity membership
-    redirect_to group
   end
 
   def destroy
-    group = find_group
-    current_user.leave group
-    redirect_to group
+    @group = find_group
+    current_user.leave @group
   end
 
   private
