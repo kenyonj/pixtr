91c80c07f9c93eeed423275c98b08cf190d822bb
Justin Kenyon <kenyonj@gmail.com>
2014-04-02 17:43:24 -0400

    fix gallery model spelling

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -4,7 +4,7 @@ class Gallery < ActiveRecord::Base
   has_many :images, dependent: :destroy
   belongs_to :user
 
-  has_many :activites, as: :subject, dependent: :destroy
+  has_many :activities, as: :subject, dependent: :destroy
 
   validates :name, presence: true
   validates :user, presence: true
