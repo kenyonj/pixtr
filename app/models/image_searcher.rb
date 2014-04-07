0e19dd62e770a637d07e2e0f858ac87ea79f55e9
Justin Kenyon <kenyonj@gmail.com>
2014-04-07 13:19:13 -0400

    Refactor ImagesController

--- app/models/image_searcher.rb
+++ app/models/image_searcher.rb
@@ -3,16 +3,20 @@ class ImageSearcher
     @query = query
   end
 
-
   def images
     image_ids = find_tags_with_id
-    Image.where("name ILIKE :query OR description ILIKE :query OR id IN (:image_ids)", query: "%#{@query}%", image_ids: image_ids)
+    Image.where(
+      "name ILIKE :query OR description ILIKE :query OR id IN (:image_ids)",
+      query: "%#{query}%",
+      image_ids: image_ids
+    )
   end
 
   private
+  attr_reader :query
 
   def find_tags_with_id
-    tags = Tag.search(@query)
+    tags = Tag.search(query)
     TagMembership.where(tag_id: tags).pluck(:image_id)
   end
 end
