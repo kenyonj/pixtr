df613fea7c8c022c0ad053cb0f67ce5351e850cd
Justin Kenyon <kenyonj@gmail.com>
2014-05-05 11:01:30 -0400

    fix space

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,7 +3,7 @@ class Activity < ActiveRecord::Base
   belongs_to :subject, polymorphic: true
 
   belongs_to :actor, class_name: "User"
-  belongs_to :target, polymorphic:true
+  belongs_to :target, polymorphic: true
 
   def self.recent
     order(created_at: :desc)
